56691b555fe09a5f6e73927c02fcef05298312c2
Ali Gardezi <ali.gardezi@tintash.com>
2022-03-23 20:26:21 +0500

    register multiple markers on map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
+import { marker, popup } from 'leaflet';
+
+const LocationMarker = () => {
+  const [position, setPosition] = useState([
+    {
+      lat: 51.505, 
+      lng: -0.09,
+    }
+  ])
+  const map = useMapEvents({
+    
+    click(e) {
+      console.log(e)
+      console.log(position)
+      setPosition([...position,e.latlng])
+
+    },
+  })
+
+  return (
+    <>
+      {
+        position.map((marker, i) => (  
+          <Marker key={`marker-${i}`} position={marker}>
+            <Popup>
+              <span>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </span>
+            </Popup>
+          </Marker>
+        ))
+      }
+    </>
+  )
+}
 
 const App = () => {
-  let z = 8;
   let x = 127;
+  let z = 8;
   let y = 25;
+  const {markers, setMarkers } = useState([[51.505, -0.09]])
+
   return (
     <div className="App">
       <div id ="map">
         <MapContainer center={[51.505, -0.09]} zoom={13} style={{height:"100%"}}>
-        <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-          <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-          </MapContainer>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {/* {markers.map((marker) => {
+            <Marker>
+              <popup>Hello</popup>
+            </Marker>
+          })} */}
+
+          <LocationMarker />
+        </MapContainer>
       </div>
     </div>
   );
