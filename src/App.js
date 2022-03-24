9c5ef329dc08f8079552b5527f18137b64ffa7e7
fozangardezi <fozangardezi@gmail.com>
2022-03-24 10:45:40 +0500

    removing unnecessary file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState,useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
 import * as L from "leaflet";
